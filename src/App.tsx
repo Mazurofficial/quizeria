e28a949fb9e286e80938921a686ff94e547daa93
Vlad Mazurenko <vladmazurenkog@gmail.com>
2023-02-20 17:41:50 +0200

    Added routing, create mainPage and cardComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
 import './App.css';
 import { QuizPage } from './pages/QuizPage';
 import { ResultsPage } from './pages/ResultsPage';
+import { MainPage } from './pages/MainPage';
 
 function App() {
    return (
       <div className="App">
          <div className="wrapper">
-            <QuizPage />
-            <ResultsPage />
+            <Routes>
+               <Route path="/" element={<MainPage />} />
+               <Route path="/quiz" element={<QuizPage />} />
+               <Route path="/results" element={<ResultsPage />} />
+            </Routes>
          </div>
       </div>
    );
