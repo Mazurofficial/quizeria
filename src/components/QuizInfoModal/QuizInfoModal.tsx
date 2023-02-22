c72e5f9a5febd0592271a422db8c50f6a5d45d44
denchikchor <59959537+denchikchor@users.noreply.github.com>
2023-02-22 19:21:39 +0200

    add style to modal window

--- src/components/QuizInfoModal/QuizInfoModal.tsx
+++ src/components/QuizInfoModal/QuizInfoModal.tsx
@@ -2,6 +2,7 @@ import { getQuizById } from '../../api';
 import { useAppSelector } from '../../app/hooks';
 import { selectActiveQuizId } from '../../features/activeQuiz/activeQuiz-slice';
 import { quizes } from '../../mock';
+import { Button } from '../../ui/Button';
 import styles from './QuizInfoModal.module.scss';
 
 export const QuizInfoModal = () => {
@@ -13,8 +14,39 @@ export const QuizInfoModal = () => {
 
    return (
       <div className={styles.quizInfoModal}>
-         <img src={img} alt={title} />
-         <h3>{title}</h3>
+         <div className={styles.quizInfoModal_Heading}>
+            <img src={img} alt={title} />
+            <div className={styles.quizInfoModal_Heading_Section}>
+               <h3>{title}</h3>
+               <Button  isLink={true} link="#">Start </Button>
+            </div>
+         </div>
+         <div className={styles.quizInfoModal_Info}>
+            <div className={styles.quizInfoModal_Info_Description}>
+               <h4>Description</h4>
+               <p>{description}</p>
+            </div>
+            <div className={styles.quizInfoModal_Info_Badges}>
+               <table>
+                  <tr>
+                     <td> <h4>Difficulty:</h4></td>
+                     <td><p>{difficulty}</p></td>
+                  </tr>
+                  <tr>
+                     <td> <h4>Category:</h4></td>
+                     <td><p>{category}</p></td>
+                  </tr>
+                  <tr>
+                     <td> <h4>Questions:</h4></td>
+                     <td><p>20</p></td>
+                  </tr>
+                  <tr>
+                     <td> <h4>Type:</h4></td>
+                     <td><p>{type}</p></td>
+                  </tr>
+               </table>
+            </div>
+         </div>
       </div>
    );
 };
